2a7ee7afbcca0f103200e4f10483ef0e6ed85453
oelemuo <obinna.elemuo@howardcc.edu>
2025-02-27 15:22:13 -0500

    added code for generating Providers and Visits

--- .venv/generateSQL.py
+++ .venv/generateSQL.py
@@ -1,3 +1,10 @@
+"""
+Python Script to generate synthetic data
+- creates 500 new patients, 100 new providers, and 500 new visits
+-ensures realistic zip codes, medical conditions, and timestamps
+- outputs a SQL file to be run in SQL server
+"""
+
 import random
 import faker
 import datetime
@@ -26,3 +33,22 @@ for patient_id in range(11, 511):
                           f"VALUES ({patient_id}, '{first_name}','{last_name}', {birth_year}, '{gender}', '{zip_code}');")
 
 
+# generate Prpviders (100)
+for provider_id in range(11, 111):
+    provider_name = f"Dr. {fake.first_name()} {fake.last_name()}"
+    specialty = random.choice(specialties)
+    hospital_affiliation = fake.company()
+    sql_statements.append(f"INSERT INTO Providers (provider_id, provider_name, specialty, hospital_affiliation)" f"VALUES "
+                          f"({visit_id}, {patient_id}, {provider_id}, {community_data_id}, '{visit_id}','{condition}',);")
+
+#generate Visits (500)
+for visit_id in range(11, 511):
+    patient_id = random.randint(1, 510) # must match generated patient IDs
+    provider_id = random.randint(1, 110)# must match generated provider IDs
+    community_data_id = random.randint(1, 10) # keep within existing community data
+    visit_date = fake.date_time_between(start_date="-1y", end_date="now").strftime("%Y-%m-%d %H:%M:%S")
+    condition = random.choice(medical_conditions)
+    sql_statements.append(f"INSERT INTO Visits (visit_id, patient_id, provider_id, community_date_id, visit_date, condition) "
+                          f"VALUES ({visit_id}, {patient_id}, {provider_id}, {community_data_id}, '{visit_date}', '{condition}');")
+
+    #Save SQL script to a file path
