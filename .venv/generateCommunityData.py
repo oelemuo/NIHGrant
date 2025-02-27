20e9c3da9b8c949c78cc384dbc0c6bb99fa8248b
oelemuo <obinna.elemuo@howardcc.edu>
2025-02-27 17:34:41 -0500

    getting rid of duplicate PKL

--- .venv/generateCommunityData.py
+++ .venv/generateCommunityData.py
@@ -18,6 +18,7 @@ fake = faker.Faker()
 weather_conditions = ["Snowstorm", "Cold", "Heatwave", "Sunny", "Blizzard", "Windy", "Flooding", "Thunderstorm",
                       "Hurricane Warning", "Heavy Rain"]
 food_insecurity_levels = ["Low", "Medium", "High"]
+
 housing_instability_levels = ["Low", "Medium", "High"]
 
 #generate sql statements
@@ -30,8 +31,16 @@ for community_data_id in range(1,101):
     food_insecurity = random.choice(food_insecurity_levels)
     housing_instability = random.choice(food_insecurity)
 
-    sql_statements.append(f"INSERT INTO Community_Data (community_data_id, zip_code, weather_condition, food_insecurity_level,"
-                          f"housing_instability_level, ) VALUES ({community_data_id}, '{zip_code}','{weather}', '{food_insecurity}', '{housing_instability}')")
+    sql_statements.append(f"MERGE INTO Community_Data AS target "
+                          f"USING (SELECT {community_data_id} AS community_data_id) AS source "
+                          f"ON target.community_data_id = source.community_data_id "
+                          f"WHEN MATCHED THEN "
+                          f"UPDATE SET zip_code = '{zip_code}', weather_condition = '{weather}', "
+                          f"food_insecurity_level = '{food_insecurity}', housing_instability_level = '{housing_instability}' "
+                          f"WHEN NOT MATCHED THEN "
+                          f"INSERT (community_data_id, zip_code, weather_condition, food_insecurity_level, housing_instability_level) "
+                          f"VALUES ({community_data_id}, '{zip_code}', '{weather}', '{food_insecurity}', '{housing_instability}');")
+
 # Saving SQL script as a file
 sql_file_path = r"C:\Users\obinn\Documents\insert_community_data.sql"
 with open(sql_file_path, "w", encoding="utf-8") as sql_file:
